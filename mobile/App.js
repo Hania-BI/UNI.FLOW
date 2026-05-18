de3f805feaceae6ad17b78ebd75ce3f46be995f8
Maryam Nasser <maryamnasser833@gmail.com>
2026-05-18 17:51:46 +0300

    Removed Unnecessary File

--- mobile/App.js
+++ mobile/App.js
@@ -12,7 +12,6 @@ import SplashScreen          from './src/screens/SplashScreen';
 import LoginScreen           from './src/screens/LoginScreen';
 import SignupScreen          from './src/screens/SignupScreen';
 import ForgotPasswordScreen  from './src/screens/ForgotPasswordScreen';
-import OTPVerificationScreen from './src/screens/OTPVerificationScreen';
 import ResetPasswordScreen   from './src/screens/ResetPasswordScreen';
 
 // App screens
@@ -129,7 +128,6 @@ function RootNavigator() {
           <Stack.Screen name="Login"           component={LoginScreen} />
           <Stack.Screen name="Signup"          component={SignupScreen} />
           <Stack.Screen name="ForgotPassword"  component={ForgotPasswordScreen} />
-          <Stack.Screen name="OTPVerification" component={OTPVerificationScreen} />
           <Stack.Screen name="ResetPassword"   component={ResetPasswordScreen} />
         </>
       )}
